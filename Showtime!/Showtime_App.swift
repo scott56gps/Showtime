fd40fbe54f405720a40cf83a3babc5685b5b8e0b
Scott Nicholes <scott93gps@gmail.com>
2021-04-23 00:12:42 -0500

    Initial Commit

---
+++ Showtime!/Showtime_App.swift
@@ -0,0 +1,17 @@
+//
+//  Showtime_App.swift
+//  Showtime!
+//
+//  Created by Scott Nicholes on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Showtime_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
