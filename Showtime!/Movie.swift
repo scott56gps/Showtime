b8e1de707ad2a7d32843fedb56bcf619775a9a1b
Scott Nicholes <scott93gps@gmail.com>
2021-04-23 00:40:53 -0500

    Initial Commit.  Added Movie Model object.

---
+++ Showtime!/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  Showtime!
+//
+//  Created by Scott Nicholes on 4/23/21.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    var title: String
+    var posterUrl: URL?
+}
