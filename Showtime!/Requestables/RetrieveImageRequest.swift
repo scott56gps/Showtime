2b2655402bbfa00807f041e9824d583588ef06f7
Scott Nicholes <scott93gps@gmail.com>
2022-01-07 03:43:40 -0800

    Added CodableImage and Retrieve Image Request

---
+++ Showtime!/Requestables/RetrieveImageRequest.swift
@@ -0,0 +1,22 @@
+//
+//  RetrieveImageRequest.swift
+//  Showtime
+//
+//  Created by Scott Nicholes on 1/7/22.
+//
+
+import Networker
+import UIKit
+
+struct RetrieveImageRequest: Requestable {
+    typealias ResultType = CodableImage
+    var path: String
+    var method: HTTPMethod = .get
+    var contentType = ""
+    var body: [String : Any]? = nil
+    var headers: [String : String]? = nil
+    
+    init(path: String) {
+        self.path = path
+    }
+}
